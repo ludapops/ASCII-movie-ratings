977feca2e85b25c2222a493ece35b153807e9078
Marc Mathieu <goheat631@gmail.com>
2015-01-16 09:47:33 -0500

    changed movie ratings

--- movieRatings.rb
+++ movieRatings.rb
@@ -22,22 +22,77 @@ end
 
 movie1_rating = ratings[0].round
 movie2_rating = ratings[1].round
+movie3_rating = ratings[2].round
+
 
 ghostBusters = rater(movie1_rating)
 dieHard = rater(movie2_rating)
+titanic = rater(movie3_rating)
 
-print ghostBusters
-puts "\n"
-puts "\n"
-puts "\n"
-print dieHard
+
+# puts "\n"
+# puts "\n"
+# puts "\n"
+
 
 class Printer 
 	def initialize(array)
 		@ratings_array = array
+		@max = array.max
+		@max = @max.round  
 	end 
 
-	def max
-		@ratings_array.max 
+	def line_printer(second_index)
+		
+
+    	line = ""
+
+    	# puts "Line ##{second_index}"
+    	@ratings_array.each do |movieRRRRating|
+    		# puts "    #{movieRRRRating} #{movieRRRRating < second_index}"
+    		if movieRRRRating < second_index
+    			line += " "
+    		else 
+    			line += "*"
+    		end
+
+    	end
+    	return line 
+
+    end
+
+
+
+	
+
+	def graph_printer
+		(1..@max).each do |number|
+    		# stuff with number
+    		second_index = @max - number
+    		puts line_printer(second_index)
+    	end
+
 	end
 end 
+
+x = Printer.new(ratings)
+x.graph_printer
+# #############################
+# # if movie rating is less than outer loop index print space
+
+
+# start at zero count up to max 
+# i < max + 1
+
+# for number in 1..10
+#     # stuff
+# end
+
+
+
+
+
+
+
+
+
